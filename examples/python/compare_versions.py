c6776c6b97334faa6ffe3070a4b3a4e46e86fbd6
Daniel Alley <dalley@redhat.com>
2026-05-23 11:19:24 -0400

    Add a CLI example

---
+++ examples/python/compare_versions.py
@@ -0,0 +1,20 @@
+"""Compare two NEVRA strings and print which is newer."""
+
+import argparse
+
+from rpm_version import Nevra
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("nevra1", help="First NEVRA string")
+parser.add_argument("nevra2", help="Second NEVRA string")
+args = parser.parse_args()
+
+a = Nevra.parse(args.nevra1)
+b = Nevra.parse(args.nevra2)
+
+if a < b:
+    print(f"{a} < {b}")
+elif a == b:
+    print(f"{a} == {b}")
+else:
+    print(f"{a} > {b}")
